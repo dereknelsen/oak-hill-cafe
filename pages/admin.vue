8a9c46856751e786ddf10889144fc47f36c11995
dereknelsen <dereksnelsen@gmail.com>
2018-06-09 12:19:47 -0400

    cms

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,29 +1,11 @@
 <template>
-	
+	<section></section>
 </template>
 
 <script>
-import CMS from 'netlify-cms'
 
-// Now the registry is available via the CMS object.
-CMS.registerPreviewTemplate('my-template', MyTemplate)
 
 export default {
-	script: [
-        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
-	],
-	mounted: [
-		{
-			if (window.netlifyIdentity) {
-				window.netlifyIdentity.on("init", user => {
-				if (!user) {
-					window.netlifyIdentity.on("login", () => {
-					document.location.href = "/admin/";
-					});
-				}
-				});
-			}
-		}
-	]
+	
 }
 </script>
