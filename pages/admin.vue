5d5182b6b2d5a8b948ac9dc09f0a61d7f8dd5c44
dereknelsen <dereksnelsen@gmail.com>
2018-06-09 11:19:35 -0400

    test

---
+++ pages/admin.vue
@@ -0,0 +1,14 @@
+<template>
+	
+</template>
+
+<script>
+import CMS from 'netlify-cms'
+
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate('my-template', MyTemplate)
+
+export default {
+	
+}
+</script>
