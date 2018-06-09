fea5c6e469819474e1bbf9683b5f047cd03115d4
Derek Nelsen <dereksnelsen@Dereks-iMac.local>
2018-06-09 18:34:06 -0400

    config

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,12 @@
         </nuxt-link>
       </li>
     </ul>
+    <h2>Gallery</h2>
+      <ul>
+        <li v-for="thumbnail in thumbnails" :key="thumbnail.date">
+          <img :src="thumbnail.image" alt="">
+        </li>
+      </ul>
   </section>
 </template>
 
@@ -19,47 +25,29 @@ export default {
     AppLogo
   },
   data() {
-    // Using webpacks context to gather all files from a folder
-    const context = require.context('~/content/blog/posts/', false, /\.json$/);
 
-    const posts = context.keys().map(key => ({
-      ...context(key),
+    // Using webpacks context to gather all files from a folder
+    const blog = require.context('~/content/blog/posts/', false, /\.json$/);
+    const gallery = require.context('~/content/gallery/thumbnails/', false, /\.json$/);
+
+    const posts = blog.keys().map(key => ({
+      ...blog(key),
       _path: `/blog/${key.replace('.json', '').replace('./', '')}`
     }));
 
-    return { posts };
+    const thumbnails = gallery.keys().map(key => ({
+      ...gallery(key),
+      _path: `/gallery/${key.replace('.json', '').replace('./', '')}`
+    }));
+
+    return { 
+      posts, 
+      thumbnails 
+    };
   }
 };
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
