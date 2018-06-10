d790d5129653a557a8dccd6e7587b0815624da07
Derek Nelsen <dereksnelsen@Dereks-iMac.local>
2018-06-09 22:49:32 -0400

    push

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,10 +5,10 @@ var path = require('path')
 // automatically and match it to the path of your Nuxt routes.
 // The Nuxt routes are generate by Nuxt automatically based on the pages folder.
 var dynamicRoutes = getDynamicPaths({
-  '/blog': 'authors/*.json',
+  '/authors': 'authors/*.json',
   '/blog': 'blog/posts/*.json',
-  '/blog': 'events/*.json',
-  '/blog': 'gallery/*.json'
+  '/events': 'events/*.json',
+  '/gallery': 'gallery/*.json'
 });
 
 
